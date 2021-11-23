344d81158c5e13588bec26f04faa5ecf88928b4b
getman <kgetmanskii@gmail.com>
2021-11-23 19:59:03 +0200

    simplified the whole program; introduced coordinates instead of counters, tests now pass

--- src/main/java/com/getman/PinSimplifier.java
+++ src/main/java/com/getman/PinSimplifier.java
@@ -3,8 +3,8 @@ package com.getman;
 public class PinSimplifier {
     private final int SIDE_LENGTH = 3;
     private final int[][] numpad;
-    private int startCol = 1;
-    private int startRow = 1;
+    private int x = 1;
+    private int y = 1;
 
     public static void main(String[] args) {
         String[] instructions = {
@@ -29,80 +29,33 @@ public class PinSimplifier {
     public int pin(String[] instructions) {
         int pin = 0;
         for(int i = 0; i < instructions.length; i++){
-            int[] directions = simplify(instructions[i]);
-            int digit = determinePinDigitAndResetStartingLocation(directions);
+            countCoordinates(instructions[i]);
+            int digit = numpad[y][x];
             pin = append(pin, digit);
         }
         return pin;
     }
 
-    private int[] simplify(String instruction) {
-        int[] steps = countSteps(instruction);
-        int rightCounter = steps[0];
-        int upCounter = steps[1];
-
-        int[] directions = {setEffectiveValue(rightCounter), setEffectiveValue(upCounter)};
-        return directions;
-    }
-
-    private int[] countSteps(String instruction) {
-        int upCounter = 0;
-        int rightCounter = 0;
-
+    private void countCoordinates(String instruction) {
         for(int i = 0; i < instruction.length(); i++) {
             char letter = instruction.charAt(i);
             switch (letter) {
-                case 'R':
-                    rightCounter++;
-                    break;
                 case 'L':
-                    rightCounter--;
+                    if(x != 0) x--;
+                    break;
+                case 'R':
+                    if(x != 2) x++;
                     break;
                 case 'U':
-                    upCounter++;
+                    if(y != 0) y--;
                     break;
                 case 'D':
-                    upCounter--;
+                    if(y != 2) y++;
                     break;
                 default:
                     break;
             }
         }
-
-        int[] inputDirections = {rightCounter, upCounter};
-        return inputDirections;
-    }
-
-    private int setEffectiveValue(int counter) {
-        if(counter >= SIDE_LENGTH) return SIDE_LENGTH - 1;
-        if(counter <= -SIDE_LENGTH) return -SIDE_LENGTH + 1;
-        return counter;
-    }
-
-    private int determinePinDigitAndResetStartingLocation(int[] directions) {
-        int col = determineColumn(directions[0]);
-        int row = determineRow(directions[1]);
-
-        startCol = col;
-        startRow = row;
-
-        return numpad[row][col];
-    }
-
-    private int determineColumn(int steps) {
-        int column = startCol + steps;
-        if(column < 0) column = 0;
-        if(column >= SIDE_LENGTH) column = SIDE_LENGTH - 1;
-
-        return column;
-    }
-
-    private int determineRow(int steps) {
-        int row = startRow - steps;
-        if(row < 0) row = 0;
-        if(row >= SIDE_LENGTH) row = SIDE_LENGTH - 1;
-
-        return row;
     }
 
     private int append(int pin, int digit) {
